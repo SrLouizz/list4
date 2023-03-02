3cd2a504e4a8b8c4bec4c5ca030d7081d727418b
Louizz <125670065+SrLouizz@users.noreply.github.com>
2023-03-02 16:13:40 -0300

    Add files via upload

---
+++ semanas.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	setlocale (LC_ALL, "Portuguese");
+	int num
+	cout << "Informe a semnada";
+	cin >> num
+	
+	case 1; cout << "Domingo";
+	break;
+	case 2; cout << "Segunda";
+	break;
+	case 3; cout << "Terça";
+	break;
+	case 4; cout << "Quarta";
+	break;
+	case 5; cout << "Quinta";
+	break;
+	case 6; cout << "Sexta";
+	break;
+	case 7; cout << "Sabado";
+	break;
+	defalt: cout << "Cabo a semana"
+}
